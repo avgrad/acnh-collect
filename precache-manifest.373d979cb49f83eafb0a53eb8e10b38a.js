702aedf99915cb3980fb13183b49be135de5cb51
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2020-04-10 19:00:04 +0200

    Deploy 1.0.1 to gh-pages

---
+++ precache-manifest.373d979cb49f83eafb0a53eb8e10b38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef9b87cd04afb9b94ceed22c743d423",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44f4244bdf39537fe8e6",
+    "url": "./static/css/main.b6b6a992.chunk.css"
+  },
+  {
+    "revision": "596f67de7677757330d6",
+    "url": "./static/js/2.5699aa24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5699aa24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f4244bdf39537fe8e6",
+    "url": "./static/js/main.9ea76ec1.chunk.js"
+  },
+  {
+    "revision": "921b64a3cf3ab53b25ae",
+    "url": "./static/js/runtime-main.43ba4ac0.js"
+  }
+]);
