0cc77abbd46c39f2de75bc177483733d5f002765
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2020-10-11 12:45:55 +0200

    display stats as fraction * with superscript, subscript and fraction slash

--- src/StatsCtrl.js
+++ src/StatsCtrl.js
@@ -2,27 +2,28 @@ import React from "react";
 import { useCollection } from "./useCollection";
 import lang from "./resources";
 
+function StatCounter({ label, donated, all }) {
+    return (
+        <li>
+            {label} <sup>{donated}</sup>
+            <span className="divider">&frasl;</span>
+            <sub>{all}</sub>
+        </li>
+    );
+}
+
 export default function StatsCtrl({ ...rest }) {
     const { stats } = useCollection();
     return (
         <ul className="stats">
-            <li>
-                {lang.entryType.BUG} {stats.bugs.donated} / {stats.bugs.all}
-            </li>
-            <li>
-                {lang.entryType.FISH} {stats.fish.donated} / {stats.fish.all}
-            </li>
-            <li>
-                {lang.entryType.FOSSIL} {stats.fossils.donated} /{" "}
-                {stats.fossils.all}
-            </li>
-            <li>
-                {lang.entryType.SEACREATURE} {stats.seaCreatures.donated} /{" "}
-                {stats.seaCreatures.all}
-            </li>
-            <li>
-                {lang.entryType.ART} {stats.art.donated} / {stats.art.all}
-            </li>
+            <StatCounter label={lang.entryType.BUG} {...stats.bugs} />
+            <StatCounter label={lang.entryType.FISH} {...stats.fish} />
+            <StatCounter label={lang.entryType.FOSSIL} {...stats.fossils} />
+            <StatCounter
+                label={lang.entryType.SEACREATURE}
+                {...stats.seaCreatures}
+            />
+            <StatCounter label={lang.entryType.ART} {...stats.art} />
         </ul>
     );
 }
