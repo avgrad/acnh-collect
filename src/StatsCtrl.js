97c12203bc46f509f9b9497ecb45c7ae6c41827c
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2020-10-11 12:29:21 +0200

    donated-stats are now a list * removed seperators and improved spacing

--- src/StatsCtrl.js
+++ src/StatsCtrl.js
@@ -5,28 +5,24 @@ import lang from "./resources";
 export default function StatsCtrl({ ...rest }) {
     const { stats } = useCollection();
     return (
-        <div className="stats">
-            <span>
+        <ul className="stats">
+            <li>
                 {lang.entryType.BUG} {stats.bugs.donated} / {stats.bugs.all}
-            </span>
-            <span className="seperator" />
-            <span>
+            </li>
+            <li>
                 {lang.entryType.FISH} {stats.fish.donated} / {stats.fish.all}
-            </span>
-            <span className="seperator" />
-            <span>
+            </li>
+            <li>
                 {lang.entryType.FOSSIL} {stats.fossils.donated} /{" "}
                 {stats.fossils.all}
-            </span>
-            <span className="seperator" />
-            <span>
+            </li>
+            <li>
                 {lang.entryType.SEACREATURE} {stats.seaCreatures.donated} /{" "}
                 {stats.seaCreatures.all}
-            </span>
-            <span className="seperator" />
-            <span>
+            </li>
+            <li>
                 {lang.entryType.ART} {stats.art.donated} / {stats.art.all}
-            </span>
-        </div>
+            </li>
+        </ul>
     );
 }
