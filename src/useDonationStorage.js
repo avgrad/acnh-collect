29289004e0178d40437d1fe0a02453cf960147f4
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2021-05-25 17:54:59 +0200

    auto-upgrade donated-storage from ids to filenames

--- src/useDonationStorage.js
+++ src/useDonationStorage.js
@@ -1,17 +1,18 @@
-import {
-    mappingFilenameToId,
-    mappingIdToFilename,
-} from "./data/collectionDataAcnhApiMapping";
+import { useEffect } from "react";
+import { mappingIdToFilename } from "./data/collectionDataAcnhApiMapping";
 import { useLocalStorageReducer } from "./helpers/useLocalStorageReducer";
 
 export default function useDonationStorage() {
     const [donated, dispatchDonated] = useLocalStorageReducer(
         (state, action) => {
+            // TODO THIS LINE IS TO BE REMOVED SOME TIME, WHEN USERS ARE EXPECTED TO HAVE "UPGRADED" THE STORAGE
+            state = state.map((id) => mappingIdToFilename[id] || id);
+
             switch (action.type) {
                 case "SET_DONATED":
-                    const idx = state.indexOf(action.id);
+                    const idx = state.indexOf(action.filename);
                     if (idx === -1 && action.donated === true)
-                        return [...state, action.id];
+                        return [...state, action.filename];
                     else if (idx !== -1 && action.donated === false) {
                         const newState = [...state];
                         newState.splice(idx, 1);
@@ -29,9 +30,18 @@ export default function useDonationStorage() {
     const setDonated = (filename, donated) =>
         dispatchDonated({
             type: "SET_DONATED",
-            id: mappingFilenameToId[filename],
+            filename,
             donated,
         });
 
-    return [donated.map((id) => mappingIdToFilename[id]), setDonated];
+    // TODO THIS USEEFFECT IS TO BE REMOVED SOME TIME, WHEN USERS ARE EXPECTED TO HAVE "UPGRADED" THE STORAGE
+    // also remove mapping-file because then it wont be needed anymore
+    // auto upgrade stored ids to filenames
+    useEffect(() => {
+        if (donated.length > 0)
+            setDonated(mappingIdToFilename[donated[0]] || donated[0], true);
+    }, []);
+
+    // TODO THIS MAP IS TO BE REMOVED SOME TIME, WHEN USERS ARE EXPECTED TO HAVE "UPGRADED" THE STORAGE
+    return [donated.map((id) => mappingIdToFilename[id] || id), setDonated];
 }
